4bb518997f8a65c22a65df54857e0be81226dc1d
titiwut999 <54914040+titiwut999@users.noreply.github.com>
2019-12-06 16:19:54 +0700

    Add files via upload

---
+++ FunctionParamExample.py
@@ -0,0 +1,13 @@
+def addNumber(x,y):
+    print(x+y)
+def minusNumber(x,y):
+    print(x-y)
+def multiplyNumber(x,y):
+    print(x*y)
+def divideNumber(x,y):
+    print(x/y)
+
+addNumber(2,70)
+minusNumber(8,50)
+multiplyNumber(3,90)
+divideNumber(70,7)
